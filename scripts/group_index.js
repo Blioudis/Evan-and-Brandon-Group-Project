612196feecf11c5fa24052326381f58d5471aa8e
Blioudis <78106566+Blioudis@users.noreply.github.com>
2023-10-24 11:43:49 -0400

    added files

---
+++ scripts/group_index.js
@@ -0,0 +1,17 @@
+// JavaScript goes in this file, javaScript single line comments follow c/c++ standard of two frontslashes
+
+$(document).ready(function() {                                              
+
+    // all custom jQuery will go here
+
+
+    $("#MButton1").click(function() {                                       
+        $("#jQdemo").css("color","purple");                                        
+        $("#jQdemo").html("Brandon Lioudis Project Journal: I have embeded a p5 game onto the website in full screen without any borders. The next steps to take are to create more games and create a new website for these games.");     
+                                   
+    });                                                                     
+
+}); // end of jQuery block
+
+// more javaScript code not using jQuery could go here
+
